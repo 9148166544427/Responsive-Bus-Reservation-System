de765221d95b6edb20887e2aac5679b594ac8477
Anand <153190468+9148166544427@users.noreply.github.com>
2024-02-08 23:04:47 +0530

    Add files via upload

---
+++ Busbooking/footer.php
@@ -0,0 +1,62 @@
+<footer>
+        <div class="footerInfo">
+            <div class="footerContainer">
+                <div class="footerItem">
+
+                    <div class="socialInfo">
+                        <div class="mainTitle">
+                            <h4> Follow Us <span></span></h4>
+                        </div>
+                        <div class="socialItem">
+                            <div class="socialLink github">
+                                <a href="https://github.com/9148166544427" class="fab fa-github"> </a>
+                            </div>
+                            <div class="socialLink linkedin">
+                            <i class="fab fa-linkedin"> </i>
+                            </div>
+                            <div class="socialLink whatsapp">
+                            <i class="fab fa-whatsapp"></i>
+                            </div>
+                        </div>
+                    </div>
+<!--  -->
+                    <div class="socialInfo">
+                        <div class="mainTitle">
+                            <h4> Service <span></span></h4>
+                        </div>
+                        <div class="socialItem">
+                            <a href="#"> E-Services </a>
+                            <a href="#"> Ticket Reservation</a>
+                            <a href="#"> End-End Encrypted </a>
+                            <a href="#"> Happy Journey</a>
+                        </div>
+                    </div>
+<!--  -->
+                    <div class="socialInfo">
+                        <div class="mainTitle">
+                            <h4> Our Health <span></span></h4>
+                        </div>
+                        <div class="socialItem">
+                            <a href="#"> Emergency dental care </a>
+                            <a href="#"> check up</a>
+                            <a href="#"> treatment of personal diseases </a>
+                            <a href="#"> tooth extraction </a>
+                        </div>
+                    </div>
+<!--  -->
+                    <div class="socialInfo">
+                        <div class="mainTitle">
+                            <h4> Address <span></span></h4>
+                        </div>
+                        <h5 class="addressTitle">Nagavara,Bangalore-560045</h5>
+                        <div class="callInfo">
+                            <h6>Call</h6> <i class="fas fa-arrow-right"></i>
+                            <span>+021585358</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+           <center> <div class="icon">  Made with <i class="fa fa-heart pulse"></i> by <span>Anand DN</span></div></center>
+        </div>
+    </footer>
+
